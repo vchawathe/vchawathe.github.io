cb7b8f09a001ecb55050a4129bdd3987b0147f6a
Vishal Chawathe <v.chawathe@f5.com>
2020-04-15 11:04:22 -0700

    some code

--- guiserver/app.js
+++ guiserver/app.js
@@ -2,13 +2,10 @@ var express = require('express');
 var process = require('process');
 var path = require('path');
 var app = express(); // better instead
-
+var port = 3001;
 
 console.log(process.cwd());
 var index = process.cwd().lastIndexOf("/")
 var path = process.cwd().substring(0,index);
 app.use('/', express.static(path));
-
-
-console.log(path);
-app.listen(3001);
+app.listen(port, () => console.log(`Example app listening at http://localhost:${port}`))
