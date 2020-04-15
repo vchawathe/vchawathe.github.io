8d161c1a75d10b780e0175067deefab6bbfff84a
Vishal Chawathe <v.chawathe@f5.com>
2020-04-15 11:01:52 -0700

    some code

--- guiserver/app.js
+++ guiserver/app.js
@@ -1,9 +1,14 @@
-const express = require('express')
-const process = require('process')
-const path = require('path');
-const app = express()
-const port = 3001
+var express = require('express');
+var process = require('process');
+var path = require('path');
+var app = express(); // better instead
 
-app.get('/', (req, res) => res.send('Hello World!'+process.cwd()))
-app.use('/static', express.static(path.join(__dirname, 'public')))
-app.listen(port, () => console.log(`Example app listening at http://localhost:${port}`))
+
+console.log(process.cwd());
+var index = process.cwd().lastIndexOf("/")
+var path = process.cwd().substring(0,index);
+app.use('/', express.static(path));
+
+
+console.log(path);
+app.listen(3001);
